4292d20bbb8634ca680fcba8d4bad11728813f57
Kevin Van de Vyver <kevinvandevyver1@gmail.com>
2018-05-14 09:28:58 +0200

    Home page + styling

---
+++ src/app/Cities-Component/cities.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-Cities',
+    templateUrl: './cities.component.html'
+})
+
+export class CitiesComponent{
+    
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule} from 'angular-bootstrap-md'
 import { AppComponent } from './app.component';
+import { NavComponent } from './Nav-Component/Nav.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './Home-Component/home.component';
+import { CitiesComponent } from './Cities-Component/cities.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    CitiesComponent
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot([
+      { path: "home", component: HomeComponent},
+      { path: "", redirectTo: "home", pathMatch: "full" },
+      /*{ path: "**", component: PageNotFoundComponent},*/
+      { path: "cities", component: CitiesComponent },
+    ], { useHash: false })
   ],
   schemas: [ NO_ERRORS_SCHEMA],
   providers: [],
